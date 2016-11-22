15eac937542956e4799c4ef11a5b8bb1cc73a78e
tinaperica <tina.perica@gmail.com>
2016-11-21 20:03:34 -0800

    20161121

--- clustered_correlations/correlations_on_the_cluster_GO_slims_pearson_complete.R
+++ clustered_correlations/correlations_on_the_cluster_GO_slims_pearson_complete.R
@@ -1,4 +1,4 @@
-### this script does all the clusters but for a small subset (7837) of pairs per task (times 44 clusters)
+### this script does all the clusters but for a small subset (203) of pairs per task (times 44 clusters)
 
 #library(getopt, lib.loc = "/netapp/home/tperica/R/x86_64-redhat-linux-gnu-library/3.3")
 
@@ -7,38 +7,14 @@ options(stringsAsFactors = F)
 lim.points <- c(-2, 1)
 lim.ratio <- c(0.3, 3)
 percent.good.cutoff <- 0.1
-
-#### load an orf to gene name index
-orf_gene_name_index <- read.delim("orf_gene_GO_sgd_annotation.txt", head = F)
-orf_index <- unique(data.frame("orf" = orf_gene_name_index$V1, "gene_name" = orf_gene_name_index$V2))
-rm(orf_gene_name_index)
-
-#### library genes clustered
-cluster_method <- "GO_slims_pearson_complete" #
-cluster_file <- paste0("clusters/2016-11-02_", cluster_method, "_clusters.txt")
-library_clusters <- read.delim(cluster_file, head = T)
-clusters <- as.character(unique(library_clusters$cluster))
-##############
-
-
-## load preprocessed ubermap data (preprocessed so that ubermap$Gene is either a Gsp1 mutant or an ORF)
-ubermap <- read.delim("preprocessed_ubermap_all_significant.txt", head = T)
-
-all_genes_and_mutants_df <- read.delim("genes_and_mutants_to_test.txt", head = T)
-all_genes_and_mutants <- all_genes_and_mutants_df$unique.genes_and_mutants_to_test.
+load("correlations_pair_preload.RData")
 
 first_pair <- as.numeric( Sys.getenv( "SGE_TASK_ID" ) )   #
 last_pair <- as.numeric( Sys.getenv( "SGE_TASK_STEPSIZE" ) )
 
-#library_clusters <- library_clusters[library_clusters$cluster %in% clusters,]
-ubermap <- ubermap[ubermap$library %in% library_clusters$ORF,]
-
-filtered_outputfilename <- paste0(Sys.Date(), "_", cluster_method, "_", first_pair, "_filtered_correlation_network_clusters.txt")
-filtered_output_file_path <- file.path("Output", cluster_method, filtered_outputfilename)
-unfiltered_outputfilename <- paste0(Sys.Date(), "_", cluster_method, "_", first_pair, "_unfiltered_correlation_network_clusters.txt")
-unfiltered_output_file_path <- file.path("Output", cluster_method, unfiltered_outputfilename)
-
-pairs<-combn(all_genes_and_mutants, 2)
+outputfilename <- paste0(Sys.Date(), "_", ubermap[["cluster_method"]], "_", first_pair, "_correlation_network_clusters.RData")
+output_file_path <- file.path("Output", cluster_method, outputfilename)
+#output_file_path <- file.path(outputfilename)  ## for debugging purposes
 
 ### for code testing purposes
 #query1 <- "R108L"
@@ -48,20 +24,23 @@ pairs<-combn(all_genes_and_mutants, 2)
 #q2 <- query2
 #query2 <- "YGL217C"
 #query2 <- "YMR235C_TSQ172"
-filtered_correlations_df <- data.frame()
-unfiltered_correlations_df <- data.frame()
-for (i in seq_along(clusters)) {
-  cluster = clusters[i]
+correlations_df <- data.frame()
+for ( i in seq_along(ubermap[["clusters"]]) ) {
+  cluster <- ubermap[["clusters"]][i]
   for ( p in first_pair:last_pair ) {
-    query1 <- pairs[1, p]
-    query2 <- pairs[2, p]
-    temp_library_clusters <- library_clusters[library_clusters$cluster == cluster,]
+    query1 <- ubermap[["pairs"]][1, p]
+    query2 <- ubermap[["pairs"]][2, p]
+    temp_library_clusters <- ubermap[["library_clusters"]][
+      ubermap[["library_clusters"]][["cluster"]] == cluster,
+      ]
     # grep, don't match, because I need to make sure to fit Gene_uniq
     ### e.g. cases like YMR235C_TSQ172
     ### also make sure to be able to handle cases when there are multiple mutants of the same gene - grep can have more than 1 hit
-    temp.ubermap.query1 <- ubermap[ubermap$library %in% temp_library_clusters$ORF & grepl(query1, ubermap$Gene_uniq),]
+    temp.ubermap.query1 <- ubermap[["ubermap"]][
+      ubermap[["ubermap"]][["library"]] %in% temp_library_clusters[["ORF"]] & grepl( query1, ubermap[["ubermap"]][["Gene_uniq"]] ), ]
     if (length(temp.ubermap.query1[,1]) > 0) {
-      temp.ubermap.query2 <- ubermap[ubermap$library %in% temp_library_clusters$ORF & grepl(query2, ubermap$Gene_uniq),]
+      temp.ubermap.query2 <- ubermap[["ubermap"]][
+        ubermap[["ubermap"]][["library"]] %in% temp_library_clusters[["ORF"]] & grepl( query2, ubermap[["ubermap"]][["Gene_uniq"]] ), ]
       if (length(temp.ubermap.query2) > 0) {
         #### queries1 and queries2 are vectors, usually of length 1, but can be more, if e.g. more than one TS mutant for gene
         queries1 <- as.character(unique(temp.ubermap.query1$Gene_uniq))
@@ -70,38 +49,37 @@ for (i in seq_along(clusters)) {
           for (q2 in queries2) {
             temp.ubermap.q1 <- temp.ubermap.query1[temp.ubermap.query1$Gene_uniq == q1,]
             temp.ubermap.q2 <- temp.ubermap.query2[temp.ubermap.query2$Gene_uniq == q2,]
-            merged.for.cor <- merge(temp.ubermap.q1, temp.ubermap.q2, by = "library")
-            merged.for.cor <- merged.for.cor[complete.cases(merged.for.cor),]
-            names(merged.for.cor) <- c("library", "Gene_uniq_q1", "score_q1", "ORF_q1", "Gene_uniq_q2", "score_q2", "ORF_q2")
-            gene_names_q1 <- merge(orf_index, merged.for.cor, by.x = "orf", by.y = "ORF_q1" )
-            gene_names_q1 <- within(gene_names_q1, "q1" <- paste(gene_name, Gene_uniq_q1, sep = " - "))
+            gene_names_q1 <- merge(ubermap[["orf_index"]], merged.for.cor, by.x = "orf", by.y = "ORF_q1" )
+            gene_names_q1 <- merge(ubermap[["orf_index"]], temp.ubermap.q1, by.x = "orf", by.y = "ORF" )
+            gene_names_q1 <- within(gene_names_q1, "q1" <- paste(gene_name, Gene_uniq, sep = " - "))
             if (length(gene_names_q1[,1]) > 0) {
-              gene_names_q1_q2 <- merge(orf_index, gene_names_q1[, c(3,5,6,7,8,9)], by.x = "orf", by.y = "ORF_q2")
-              gene_names_q1_q2 <- within(gene_names_q1_q2, "q2" <- paste(gene_name, Gene_uniq_q2, sep = " - "))
-              gene_names_q1_q2 <- gene_names_q1_q2[, c(3, 4, 6, 7, 8)]
+              gene_names_q2 <- merge(ubermap[["orf_index"]], temp.ubermap.q2, by.x = "orf", by.y = "ORF" )
+              gene_names_q2 <- within(gene_names_q2, "q2" <- paste(gene_name, Gene_uniq, sep = " - "))
+              gene_names_q1_q2 <- merge(gene_names_q1[, c(4,5,6)], gene_names_q2[, c(4,5,6)], by = "library")
               if (length(gene_names_q1_q2[,1]) > 0) {
+                names(gene_names_q1_q2) <- c("library", "score_q1", "q1", "score_q2", "q2")
                 correlation <- cor(gene_names_q1_q2$score_q1, gene_names_q1_q2$score_q2, use="pairwise.complete.obs")
-                unfiltered_correlations_df <- rbind(
-                  unfiltered_correlations_df, 
-                  data.frame( 
-                    "gene1" = gene_names_q1_q2$q1[1], "gene_2" = gene_names_q1_q2$q2[1], 
-                    "cluster_method" = cluster_method, "cluster" = cluster,
-                    "corr"  = round(correlation, 3)
-                  ))
-                gene_names_q1_q2 <- cbind( gene_names_q1_q2, "score_ratio" = abs(gene_names_q1_q2$score_q1)/abs(gene_names_q1_q2$score_q2) )
                 diagonal.merged.for.cor <- gene_names_q1_q2[ findInterval(gene_names_q1_q2$score_q1, lim.points) != 1 & 
-                                                         findInterval(gene_names_q1_q2$score_q2, lim.points) != 1 &
-                                                        findInterval(gene_names_q1_q2$score_ratio, lim.ratio) == 1, ]
+                                  findInterval(gene_names_q1_q2$score_q2, lim.points) != 1 &
+                                  findInterval(gene_names_q1_q2$score_ratio, lim.ratio) == 1, ]
                 n.genes.on.diagonal <- length(diagonal.merged.for.cor[,1])
                 total.n.genes <- length(gene_names_q1_q2[complete.cases(gene_names_q1_q2),][,1])
                 if ( n.genes.on.diagonal/total.n.genes > percent.good.cutoff ) {
-                  filtered_correlations_df <- rbind(
-                    filtered_correlations_df, 
+                  correlations_df <- rbind(
+                    correlations_df, 
                     data.frame( 
                       "gene1" = gene_names_q1_q2$q1[1], "gene_2" = gene_names_q1_q2$q2[1], 
-                      "cluster_method" = cluster_method, "cluster" = cluster,
-                      "corr"  = round(correlation, 3)
+                      "cluster_method" = ubermap[["cluster_method"]], "cluster" = cluster,
+                      "corr"  = round(correlation, 3), "filter" = "filtered"
                     ))
+                } else {
+                  correlations_df <- rbind(
+                      correlations_df,
+                      data.frame(
+                        "gene1" = gene_names_q1_q2$q1[1], "gene_2" = gene_names_q1_q2$q2[1],
+                        "cluster_method" = ubermap[["cluster_method"]], "cluster" = cluster,
+                        "corr"  = round(correlation, 3), "filter" = "unfiltered"
+                      ))
                 }
               }
             }
@@ -111,5 +89,4 @@ for (i in seq_along(clusters)) {
     }
   }
 }
-write.table(filtered_correlations_df, file = filtered_output_file_path, quote = F, sep = "\t", row.names = F)
-write.table(unfiltered_correlations_df, file = unfiltered_output_file_path, quote = F, sep = "\t", row.names = F)
+save(correlations_df, file = output_file_path)
---
+++ clustered_correlations/preprocess_GO_slims_pearson_complete.R
@@ -0,0 +1,33 @@
+### this script does all the clusters but for a small subset (203) of pairs per task (times 44 clusters)
+
+
+options(stringsAsFactors = F)
+
+ubermap <- list()
+
+
+#### load an orf to gene name index
+orf_gene_name_index <- read.delim("../orf_gene_GO_sgd_annotation.txt", head = F)
+ubermap[["orf_index"]] <- unique(data.frame("orf" = orf_gene_name_index$V1, "gene_name" = orf_gene_name_index$V2))
+rm(orf_gene_name_index)
+
+#### library genes clustered
+ubermap[["cluster_method"]] <- "GO_slims_pearson_complete" #
+cluster_file <- paste0("clusters/2016-11-02_", cluster_method, "_clusters.txt")
+ubermap[["library_clusters"]] <- read.delim(cluster_file, head = T)
+ubermap[["clusters"]] <- as.character( unique( ubermap[["library_clusters"]][["cluster"]] ))
+##############
+
+
+## load preprocessed ubermap data (preprocessed so that ubermap$Gene is either a Gsp1 mutant or an ORF)
+ubermap[["ubermap"]] <- read.delim("../preprocessed_ubermap_all_significant.txt", head = T)
+
+
+all_genes_and_mutants_df <- read.delim("../genes_and_mutants_to_test.txt", head = T)
+ubermap[["all_genes_and_mutants"]] <- all_genes_and_mutants_df$unique.genes_and_mutants_to_test.
+rm(all_genes_and_mutants_df)
+ubermap[["pairs"]] <- combn(ubermap[["all_genes_and_mutants"]], 2)
+ubermap[["cluster_method"]] <- cluster_method
+
+
+save(ubermap, file = "correlations_pair_preload.RData")
