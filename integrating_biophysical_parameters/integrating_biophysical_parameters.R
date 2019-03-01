ca4b2571bf1f7db8514b9765a99ee800c0b48b29
tinaperica <tina.perica@gmail.com>
2019-03-01 14:34:21 -0800

    kinetic parameters

--- integrating_biophysical_parameters/integrating_biophysical_parameters.R
+++ integrating_biophysical_parameters/integrating_biophysical_parameters.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(factoextra)
 library(fastcluster)
 library(RColorBrewer)
+library(ggrepel)
+
 date <- as.character(Sys.Date())
 ### load in e.map data
 load("basic_E-MAP_data/June2016_Gsp1_E-MAP_data.RData")
@@ -14,24 +16,27 @@ e.map <- as_tibble(e.map) %>%
 ### input all the parameters for mutants
 GEF_kin <- read_tsv("hierarchical_clustering_of_parameters/GEF_kinetics_param.txt", col_names = T) %>% 
   gather("measure", "value", -mutant)
-NMR_data <- read_tsv("hierarchical_clustering_of_parameters/NMR_data_clean.txt", col_names = T) %>% 
+GAP_kin <- read_tsv("hierarchical_clustering_of_parameters/GAP_MM_parameters_clean.txt", col_names = T) %>% 
+  gather("measure", "value", -mutant)
+NMR_data <- read_tsv("hierarchical_clustering_of_parameters/20190224_nmr_data.txt", col_names = T) %>% 
   select("mutant" = "gsp1_mutation", "gamma2" = gamma2_percent) %>% 
   gather("measure", "value", -mutant)
 apms_foldchange <- read_tsv("hierarchical_clustering_of_parameters/apms_log2_fold_change.txt", col_names = T) %>% 
   select(mutant, log2FC, tag, gene_name) %>% 
   filter(tag == "N") %>% 
-  #group_by(mutant, gene_name) %>% 
-  #mutate("avg_log2FC" = mean(log2FC)) %>% 
-  #ungroup() %>% 
+  group_by(mutant, gene_name) %>% 
+  mutate("avg_log2FC" = mean(log2FC)) %>% 
+  ungroup() %>% 
   select(mutant, "measure" = gene_name, "value" = avg_log2FC) %>% 
   filter(measure %in% c("SRM1", "RNA1", "YRB1", "MOG1"))
-all_parameters <- bind_rows(GEF_kin, NMR_data, apms_foldchange)
+all_parameters <- bind_rows(GEF_kin, GAP_kin, NMR_data, apms_foldchange)
 all_parameters <- all_parameters %>% 
   ##### add log2fold change of zero for WT
   bind_rows(., tibble("measure" = c("SRM1", "RNA1", "YRB1", "MOG1"), mutant = "WT", value = rep(0, 4))) %>% 
   unique() %>% 
   group_by(measure) %>% 
-  mutate("value" = scale(value)) %>% 
+  mutate("raw_value" = value) %>% 
+  mutate("value" = scale(raw_value)) %>% 
   ungroup()
 ###########
 
@@ -96,6 +101,7 @@ cluster_by_parameters <- function(emap, biophy_params, k) {
   #### cluster by parameters
   param_subset_spread <- param_subset %>% 
     filter(mutant %in% mutants_with_all_params) %>% 
+    select(-raw_value) %>% 
     spread(measure, value)
   param_subset_spread <- data.frame(param_subset_spread)
   rownames(param_subset_spread) <- param_subset_spread$mutant
@@ -112,23 +118,76 @@ cluster_by_parameters <- function(emap, biophy_params, k) {
   dev.off()
 }
 
-available_paramteres <- all_parameters %>% pull(measure) %>% unique()
-cluster_by_parameters(emap = e.map, biophy_params = c("kcat", "Km"), k = 4)
-cluster_by_parameters(emap = e.map, biophy_params = c("kcat", "Km", "gamma2"), k = 4)
+available_parameters <- all_parameters %>% pull(measure) %>% unique()
+cluster_by_parameters(emap = e.map, biophy_params = c("GAP_kcat", "GAP_Km", "GEF_kcat", "GEF_Km"), k = 6)
+cluster_by_parameters(emap = e.map, biophy_params = c("GAP_kcat_Km", "GEF_kcat_Km"), k = 4)
+
+cluster_by_parameters(emap = e.map, biophy_params = c("GAP_kcat", "GAP_Km", "GEF_kcat", "GEF_Km", "gamma2"), k = 4)
 
 cluster_by_parameters(emap = e.map, biophy_params = c("SRM1", "RNA1"), k = 4)
 cluster_by_parameters(emap = e.map, biophy_params = c("SRM1", "RNA1", "MOG1"), k = 4)
 cluster_by_parameters(emap = e.map, biophy_params = c("SRM1", "RNA1", "YRB1"), k = 4)
 cluster_by_parameters(emap = e.map, biophy_params = c("SRM1", "RNA1", "MOG1", "YRB1"), k = 4)
-cluster_by_parameters(emap = e.map, biophy_params = c("kcat", "Km", "gamma2", "SRM1", "RNA1"), k = 4)
-cluster_by_parameters(emap = e.map, biophy_params = c("kcat", "Km", "gamma2", "SRM1", "RNA1", "MOG1", "YRB1"), k = 4)
+cluster_by_parameters(emap = e.map, biophy_params = c("GAP_kcat", "GAP_Km", "GEF_kcat", "GEF_Km", "gamma2", "SRM1", "RNA1"), k = 4)
+cluster_by_parameters(emap = e.map, biophy_params = c("GAP_kcat", "GAP_Km", "GEF_kcat", "GEF_Km", "gamma2", "SRM1", "RNA1", "MOG1", "YRB1"), k = 4)
 
 
 
+WT_parameters <- all_parameters %>% 
+  filter(mutant == "WT") %>% 
+  select(-value, -mutant) %>% 
+  rename("wt_value" = raw_value)
+all_parameters %>% filter(measure %in% c("gamma2", "GAP_kcat_Km")) %>% 
+  select(-value) %>% 
+  inner_join(., WT_parameters, by = c("measure")) %>% 
+  mutate("rel_value" = raw_value/wt_value) %>% 
+  select(-raw_value, -wt_value) %>% 
+  spread(measure, rel_value) %>% 
+  ggplot(aes(x = gamma2, y = log(GAP_kcat_Km), label = mutant)) +
+  geom_point() +
+  geom_text_repel() +
+  ylab("ln(kcat/Km(MUT) / kcat/Km(WT)) of GAP mediated GTP hydrolysis") +
+  xlab("% state 2") +
+  theme(axis.title.x = element_text(size = 15), axis.title.y = element_text(size = 15))
+ggsave(filename = "hierarchical_clustering_of_parameters/GAP_enzyme_eff_vs_gamma2.pdf", width = 12)
 
 
+rel_GAP_GEF_efficiency <- all_parameters %>% 
+  filter(measure %in% c("GEF_kcat_Km", "GAP_kcat_Km")) %>% 
+  select(-value) %>% 
+  inner_join(., WT_parameters, by = c("measure")) %>% 
+  mutate("rel_value" = raw_value/wt_value) %>% 
+  select(-raw_value, -wt_value) 
+  
+  
+rel_GAP_GEF_efficiency %>% 
+  spread(measure, rel_value) %>% 
+  ggplot(aes(x = log(GEF_kcat_Km), y = log(GAP_kcat_Km), label = mutant)) +
+  geom_point() +
+  geom_text_repel() +
+  xlab("ln(kcat/Km(MUT) / kcat/Km(WT)) of GEF mediated nucleotide exchange") +
+  ylab("ln(kcat/Km(MUT) / kcat/Km(WT)) of GAP mediated GTP hydrolysis") +
+  theme(axis.title.x = element_text(size = 15), axis.title.y = element_text(size = 15))
 
+ggsave(filename = "hierarchical_clustering_of_parameters/GAP_enzyme_eff_vs_GEF_enzyme_eff.pdf", width = 12)
 
+GAP_GEF_ratio <- rel_GAP_GEF_efficiency %>% 
+  spread(measure, rel_value) %>% 
+  mutate("GAP_GEF_ratio" = GAP_kcat_Km / GEF_kcat_Km) %>% 
+  filter(! is.na(GAP_GEF_ratio)) %>% 
+  arrange(GAP_GEF_ratio) 
+mutants_by_GAP_GEF_ratio <- GAP_GEF_ratio %>% 
+  pull(mutant) %>% unique()
+
+GAP_GEF_ratio %>% 
+  mutate("mutant" = factor(mutant, mutants_by_GAP_GEF_ratio)) %>% 
+  ggplot(aes(x = mutant, y = log(GAP_GEF_ratio))) + 
+  geom_point(size = 6, alpha = 0.5) +
+  ylab("ln(relative GAP over relative GEF efficiency)") +
+  xlab("Gsp1 point mutant") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 15),
+        axis.title = element_text(size = 15)) +
+  geom_hline(yintercept = 0, color = "red", alpha = 0.5)
 
 
 
